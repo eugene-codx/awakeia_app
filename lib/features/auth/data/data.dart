3f8821c31567f11b533eac288e83bd41de367691
Eugene N. Schneider <Eugene.N.Schneider@gmail.com>
2025-07-16 23:00:08 +0300

    add features

---
+++ lib/features/auth/data/data.dart
@@ -0,0 +1,15 @@
+// Data layer exports for auth feature
+// This file exports all data layer components
+
+// Models
+export 'models/user_model.dart';
+
+// Data sources
+// export 'datasources/auth_remote_data_source.dart';
+// export 'datasources/auth_local_data_source.dart';
+
+// Repositories
+// export 'repositories/auth_repository_impl.dart';
+
+// Mappers
+// export 'mappers/user_mapper.dart';
